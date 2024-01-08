40fc7adbb81d4ec65ca352957673ca51bc29798a
Ryan LaClaire <ryanlaclaire32@gmail.com>
2024-01-08 14:31:25 -0500

    New Initial Commit

---
+++ LongshoreFactor/Pages/LongshoreLinks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LongshoreFactor.Pages
+{
+    public class LongshoreLinksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
